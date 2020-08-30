e8e7678a08cd31c118bc1e24d0928e3efe9da4fa
Emiliano Pamont <emipmttt@gmail.com>
2020-08-30 13:37:20 -0500

    login mauckup

---
+++ src/views/Auth/Auth.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <v-card style="width:70%; overflow: hidden" class="secondary--bg mx-auto mt-10">
+      <v-row style="max-width:100%; margin auto 0 ">
+        <v-col class="pa-5">
+          <h1 class="white--text">SIGE</h1>
+        </v-col>
+        <v-col class="pa-5 white" style="box-sizing: border-box">
+          <h2>Iniciar Sesión</h2>
+          <br />
+          <v-text-field label="Correo Electrónico" dense outlined></v-text-field>
+          <v-text-field label="Contraseña" dense outlined></v-text-field>
+          <div class="d-flex">
+            <v-btn class="primary--text" text>Regístrate</v-btn>
+            <v-btn class="primary--bg white--text">Iniciar Sesión</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
